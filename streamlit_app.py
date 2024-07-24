5c9019f4639285f677c5cd94e600eff82c5e7e25
Raphael Louvrier <34351922+Raphael9999@users.noreply.github.com>
2024-07-24 11:45:22 -0500

    Update streamlit_app.py
    switch to pandas df

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -17,7 +18,11 @@ session = cnx.session()
 # .select(col( to select a specific column instead of the whole table
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"), col("SEARCH_ON"))
 # display the dataframe from above
-st.dataframe(data=my_dataframe, use_container_width=True)
+# st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+
+pd_df = my_dataframe.to_pandas()
+st.dataframe(data=pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
